dd35048072305f883fb05e4dc0a61f2bcdb6c1ce
j-blalock <justin.m.blalock@gmail.com>
2020-09-16 18:02:31 -0500

    Commit 2 - Palindrome

--- js/app.js
+++ js/app.js
@@ -10,8 +10,8 @@
 */
 
 // 1. 
-// Parameters are the placeholder names declared in the fuction.
-// Arguments are the actual values passed to and recieved by the function.
+//  Parameters are the placeholder names declared in the fuction. Arguments 
+//  are the actual values passed to and recieved by the function.
 
 // 2. 
 //  **return** ends function execution and specifies a value to be returned to
@@ -26,3 +26,27 @@
 //  to create large programs by 'dividing and conquering'. Functions reduce 
 //  repetition by outsourcing repetitive operations.  It also fosters
 //  collaboration by allowing for modularization.
+
+/*
+## 2. Palindrome again.
+Write a function `checkPalindrome` that accepts a single argument, a string. 
+Yes, you've done it before, but do it again. Later in this assignment we're 
+gonna beef up our palindrome function some. See if you can do it without 
+looking back at your previous answer. The function should return true if 
+the string is a palindrome, false if not. Make sure your function will give 
+the correct answer for words with **capital letters**.
+
+console.log(checkPalindrome("Radar"));
+    => true
+
+console.log(checkPalindrome("Borscht"));
+    => false
+*/
+
+function checkPalindrome(string) {
+    string = string.toLowerCase();
+    reverse = string.split('').reverse().join('');
+    return string == reverse;
+}
+console.log(checkPalindrome("Radar"));
+console.log(checkPalindrome("Borscht"));

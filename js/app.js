63add915e78ace68b4eefbcacd9b505294a21ecd
j-blalock <justin.m.blalock@gmail.com>
2020-09-16 17:44:11 -0500

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,28 @@
+/*
+## 1. Verbal questions
+
+    Write answers to the following questions as comments. 
+
+1.  What is the difference between a **parameter** and an **argument**?
+2.  Within a function, what is the difference between **return** and 
+    **console.log**?
+3.  What are the implications of the ability of a function to return a value?
+*/
+
+// 1. 
+// Parameters are the placeholder names declared in the fuction.
+// Arguments are the actual values passed to and recieved by the function.
+
+// 2. 
+//  **return** ends function execution and specifies a value to be returned to
+//  the function caller.  The **console** object provides access to the browser's
+//  debugging console. **.log()** is a method of the console object that outputs 
+//  a message to the console.  **return** does not display values to the
+//  console, rather it 'returns' it to whatever called it. The output value of 
+//  a function can not come from a console.log.
+
+// 3. 
+//  Functions that have the ability to return values allows programmers
+//  to create large programs by 'dividing and conquering'. Functions reduce 
+//  repetition by outsourcing repetitive operations.  It also fosters
+//  collaboration by allowing for modularization.
